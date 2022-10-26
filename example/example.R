aa18b8982e90a5d02a6c8707bb8d8da1535f9766
Lloyd Chapman <lacchapman@gmail.com>
2022-10-26 15:26:56 +0100

    Add comments to example

--- example/example.R
+++ example/example.R
@@ -1,8 +1,11 @@
 library(sir)
 
+# Set time step and initial numbers of susceptible and infected individuals
 dt <- 0.25
 S_ini <- 1000
 I_ini <- 10
+
+# Create dust object for model
 sir_model <- sir$new(
     list(dt = dt,S_ini = S_ini,I_ini = I_ini,
          beta = 0.4,gamma = 0.1),
@@ -19,6 +22,7 @@ for (t in seq_len(n_steps)) {
 time <- x[1, 1, ]
 x <- x[-1, , ]
 
+# Plot trajectories
 par(mar = c(4.1, 5.1, 0.5, 0.5), las = 1)
 cols <- c(S = "#8c8cd9", I = "#cc0044", R = "#999966")
 matplot(time, t(x[1, , ]), type = "l",
